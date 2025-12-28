f4a61c551e12fd31f5bc991e033b3223ba8539ad
Imam Alfath <imam@merri.cx>
2025-12-29 05:13:44 +0700

    Update FRI test

--- tests/test_fri.py
+++ tests/test_fri.py
@@ -1,52 +1,53 @@
 import pytest
 import random
-from zksnake.subprotocol.fri import FRI
+from zksnake.subprotocol.ldt import FRI
 from zksnake.polynomial import Polynomial
-from zksnake.commitment.polynomial.fri import FRI_PCS
 from zksnake.constant import BN254_SCALAR_FIELD
-from zksnake.transcript import FiatShamirTranscript
 
 
 def test_fri_low():
 
-    fri = FRI(16, BN254_SCALAR_FIELD, folding_factor=2, last_layer_degree_bound=2)
-
-    # pre-seeded to avoid long Proof of Work
-    random.seed("test")
-    commitment, proof = fri.prove(
-        [random.randint(1, BN254_SCALAR_FIELD - 1) for _ in range(16)]
+    fri = FRI(
+        15,
+        BN254_SCALAR_FIELD,
+        blowup_factor=2,
+        folding_factor=2,
+        last_layer_degree_bound=2,
+        num_queries=5,
     )
+    print(f"FRI security bits: {fri.get_security_bits()}")
 
-    assert fri.verify(commitment, proof)
-
-
-def test_fri_high():
-
-    fri = FRI(2048, BN254_SCALAR_FIELD, folding_factor=4, last_layer_degree_bound=1)
-
-    # pre-seeded to avoid long Proof of Work
-    random.seed("test")
-    commitment, proof = fri.prove(
-        [random.randint(1, BN254_SCALAR_FIELD - 1) for _ in range(2048)]
-    )
-
-    assert fri.verify(commitment, proof)
-
-
-def test_fri_pcs():
-
-    fri = FRI_PCS(16, BN254_SCALAR_FIELD, folding_factor=2, last_layer_degree_bound=2)
-    # pre-seeded to avoid long Proof of Work
+    # pre-seeded to avoid unpredictable long Proof of Work
     random.seed("test")
 
     poly = Polynomial(
         [random.randint(1, BN254_SCALAR_FIELD - 1) for _ in range(16)],
         BN254_SCALAR_FIELD,
     )
+    commitment, proof = fri.prove(poly)
 
-    transcript = FiatShamirTranscript(b"FRI", BN254_SCALAR_FIELD)
-    commitment, codewords = fri.commit(poly, 1337, transcript)
-    proof = fri.open(codewords, transcript)
+    assert fri.verify(commitment, proof)
 
-    transcript = FiatShamirTranscript(b"FRI", BN254_SCALAR_FIELD)
-    assert fri.verify(commitment, proof, transcript)
+
+def test_fri_high():
+
+    fri = FRI(
+        2047,
+        BN254_SCALAR_FIELD,
+        blowup_factor=16,
+        folding_factor=8,
+        last_layer_degree_bound=4,
+        num_queries=15,
+    )
+    print(f"FRI security bits: {fri.get_security_bits()}")
+
+    # pre-seeded to avoid unpredictable long Proof of Work
+    random.seed("test")
+
+    poly = Polynomial(
+        [random.randint(1, BN254_SCALAR_FIELD - 1) for _ in range(1024)],
+        BN254_SCALAR_FIELD,
+    )
+    commitment, proof = fri.prove(poly)
+
+    assert fri.verify(commitment, proof)
